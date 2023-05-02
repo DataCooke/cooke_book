39b851a6b7dd31dd86a2eaee6f0bfc4dc86b3d50
Jonathan Cooke <jonathancooke@Jonathans-MacBook-Pro.local>
2023-05-02 11:41:20 -0600

    new files

---
+++ setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='ds_toolkit',
+    version='0.0.1',
+    author='Jonathan Cooke',
+    author_email='cooke.jon.mba@gmail.com',
+    description='Data Science Functions for personal use',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/byemoney/toolkit',
+    license='MIT',
+    packages=['ds_toolkit'],
+    install_requires=['requests'],
+)
