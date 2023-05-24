4cb4c6b8cd1ba04b1a5f54025a14181efa27ecde
Jonathan Cooke <jonathancooke@Jonathans-MacBook-Pro.local>
2023-05-24 14:51:08 -0600

    change param name for increment_data

--- data_generation_book/increment_data.py
+++ data_generation_book/increment_data.py
@@ -1,13 +1,13 @@
 import pandas as pd
 
 
-def increment_data(dataframe, predictor, increment_value, n_increments, direction='both'):
+def increment_data(dataframe, target_column, increment_value, n_increments, direction='both'):
     """
-    Increment a specified predictor in the dataframe and add a new row for each increment.
+    Increment a specified target_column in the dataframe and add a new row for each increment.
     
     Parameters:
     dataframe (DataFrame): The input dataframe.
-    predictor (str): The predictor to be incremented.
+    target_column (str): The column to be incremented.
     increment_value (float): The value by which to increment the predictor.
     n_increments (int): The number of increments.
     direction (str, optional): The direction of increment ('ascending', 'descending', or 'both'). Default is 'both'.
@@ -19,8 +19,8 @@ def increment_data(dataframe, predictor, increment_value, n_increments, directio
     if not isinstance(dataframe, pd.DataFrame):
         raise ValueError("The input should be a pandas DataFrame.")
     
-    if predictor not in dataframe.columns:
-        raise ValueError(f"The predictor should be one of the dataframe's columns. Got {predictor} instead.")
+    if target_column not in dataframe.columns:
+        raise ValueError(f"The target_column should be one of the dataframe's columns. Got {predictor} instead.")
     
     if direction not in ['ascending', 'descending', 'both']:
         raise ValueError("The direction should be one of 'ascending', 'descending', or 'both'.")
@@ -30,7 +30,7 @@ def increment_data(dataframe, predictor, increment_value, n_increments, directio
     
     # Calculate the increments
     for i in range(1, n_increments + 1):
-        current_value = df[predictor].iloc[-1]
+        current_value = df[target_column].iloc[-1]
         
         if direction == 'ascending':
             increment = increment_value
@@ -41,12 +41,12 @@ def increment_data(dataframe, predictor, increment_value, n_increments, directio
         
         new_value = current_value + increment
         new_row = df.iloc[[-1]].copy()
-        new_row[predictor] = new_value
+        new_row[target_column] = new_value
         
         # Add the new row to the dataframe
         df = df.append(new_row, ignore_index=True)
     
     # Sort the dataframe
-    df.sort_values(by=predictor, inplace=True, ignore_index=True)
+    df.sort_values(by=target_column, inplace=True, ignore_index=True)
     
     return df
