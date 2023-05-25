4f33753ab10e97a2be99b79076b9ac8e755038e0
Jonathan Cooke <jonathancooke@Jonathans-MacBook-Pro.local>
2023-05-25 13:02:25 -0600

    add logging to feature elim

--- model_book/feature_elimination.py
+++ model_book/feature_elimination.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 from sklearn.metrics import mean_absolute_error
 import time
+import logging
 
 
 def feature_elimination(X, y, model, metric=mean_absolute_error, n_jobs=1):
