2a3214f6f16c798683b34d0d8168697393ce3789
Jonathan Cooke <cooke.jon.mba@gmail.com>
2023-08-07 23:00:38 -0600

    editing feature elimination and plot variable importance

--- model_book/feature_elimination.py
+++ model_book/feature_elimination.py
@@ -1,11 +1,10 @@
-import pandas as pd
 import numpy as np
-from sklearn.metrics import mean_absolute_error
-import time
+import pandas as pd
 import logging
+import time
+from sklearn.metrics import mean_absolute_error
 
-
-def feature_elimination(X, y, model, metric=mean_absolute_error, n_jobs=1):
+def feature_elimination(X, y, model, metric=mean_absolute_error, metric_direction='lower', n_jobs=1):
     """
     Perform backward feature elimination on a given model and dataset.
 
@@ -14,7 +13,8 @@ def feature_elimination(X, y, model, metric=mean_absolute_error, n_jobs=1):
     y (pd.Series): Target variable
     model (sklearn estimator): The model to use for feature importance
     metric (callable, default=mean_absolute_error): Metric to use for model evaluation. Should be a callable
-        that takes two arguments (the true values and the predicted values, in that order) and returns a scalar value where lower values are better.
+        that takes two arguments (the true values and the predicted values or predicted probabilities, in that order) and returns a scalar value.
+    metric_direction (str, default='lower'): Indicates if the metric should be minimized ('lower') or maximized ('higher').
     n_jobs (int, default=1): Number of cores to run in parallel while fitting across folds. None means 1 unless in a joblib.parallel_backend context. -1 means using all processors.
 
     Returns:
@@ -28,7 +28,11 @@ def feature_elimination(X, y, model, metric=mean_absolute_error, n_jobs=1):
     
     for i in range(num_cols):
         model.fit(X, y)
-        y_pred = model.predict(X)
+        if metric_direction == 'lower':
+            y_pred = model.predict(X)
+        else:
+            y_pred = model.predict_proba(X)[:, 1]
+        
         score = metric(y, y_pred)
         metric_scores.append(score)
         num_cols_list.append(num_cols)
@@ -46,8 +50,10 @@ def feature_elimination(X, y, model, metric=mean_absolute_error, n_jobs=1):
             remaining_col = X.columns[-1]
             break
 
+    metric_name = metric.__name__
+
     feature_elim_df = pd.DataFrame({
-        'metric_score': metric_scores,
+        metric_name: metric_scores,
         'num_cols': num_cols_list,
         'eliminated_feature': eliminated_features
     })
--- model_book/plot_variable_importance.py
+++ model_book/plot_variable_importance.py
@@ -1,29 +1,40 @@
 import matplotlib.pyplot as plt
 import logging
 
-
-def plot_variable_importance(df, metric_name='Metric Score'):
+def plot_variable_importance(df, metric_name=None):
     """
     Plot the performance of the model at each step of the feature elimination.
 
     Parameters:
     df (pd.DataFrame): DataFrame containing the metric scores, number of features, and weakest feature at each step
-    metric_name (str, default='Metric Score'): Name of the metric used for model evaluation. Used for y-axis label in the plot.
+    metric_name (str, default=None): Name of the metric used for model evaluation. Used for y-axis label in the plot.
 
     Returns:
     None
     """
     try:
+        # If metric_name is not provided, infer it from the first column of the DataFrame
+        if metric_name is None:
+            metric_name = df.columns[0]
+        
         fig, ax = plt.subplots(figsize=(10, 6))
         sorted_df = df.sort_values(by='num_cols', ascending=False)
         sorted_df['count'] = range(1, len(sorted_df) + 1)
-        ax.plot(sorted_df['count'], sorted_df['metric_score'], 'o-', markersize=8)
+        ax.plot(sorted_df['count'], sorted_df[metric_name], 'o-', markersize=8)
         ax.set_xticks(sorted_df['count'])
         ax.set_xticklabels(sorted_df.index[::-1], rotation=90)
         ax.set_xlabel('Features')
         ax.set_ylabel(metric_name)
         ax.set_title('Feature Selection')
-        ax.axhline(sorted_df['metric_score'].min(), color='gray', linestyle='--', linewidth=1.5)
+        
+        # Determine whether to highlight max or min based on metric_name
+        if any(word in metric_name.lower() for word in ['error', 'loss']):
+            ax.axhline(sorted_df[metric_name].min(), color='gray', linestyle='--', linewidth=1.5)
+        elif any(word in metric_name.lower() for word in ['accuracy', 'roc_auc', 'precision', 'recall', 'f1']):
+            ax.axhline(sorted_df[metric_name].max(), color='gray', linestyle='--', linewidth=1.5)
+        else:
+            logging.warning("Unable to determine whether higher or lower metric values are better. No reference line added.")
+            
         ax.invert_xaxis()  # flip the x-axis
         plt.show()
     except Exception as e:
