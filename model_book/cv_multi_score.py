40e21e25033f0084277a595a4c28bf4335c7e223
Jonathan Cooke <cooke.jon.mba@gmail.com>
2023-08-07 22:33:48 -0600

    add classification to cv_multi_score

--- model_book/cv_multi_score.py
+++ model_book/cv_multi_score.py
@@ -1,9 +1,8 @@
 import pandas as pd
 import logging
-from sklearn.model_selection import cross_validate
+from sklearn.model_selection import cross_validate, StratifiedKFold, KFold
 
-
-def cv_multi_score(model, predictors, target, scoring, folds):
+def cv_multi_score(model, predictors, target, scoring=None, folds=5, task='regression'):
     """
     Calculate cross-validation scores for a given model.
 
@@ -16,14 +15,28 @@ def cv_multi_score(model, predictors, target, scoring, folds):
         with keys being the scorer names and values the scorer callables.
     folds (int or cross-validation generator): The number of folds in K-fold 
         cross validation, or a cross-validation generator.
+    task (str): The type of machine learning task - 'classification' or 'regression'.
 
     Returns:
     scores (dict): A dictionary of the mean and standard deviation of the 
         test scores for each scorer.
 
     """
+    # Set default scoring metrics based on the 'task' parameter
+    if scoring is None:
+        if task == 'classification':
+            scoring = ['accuracy', 'roc_auc', 'precision', 'recall', 'f1']
+        else:
+            scoring = ['neg_mean_absolute_error', 'neg_median_absolute_error', 'r2']
+
+    # If it's a classification task, use StratifiedKFold
+    if task == 'classification':
+        cv = StratifiedKFold(n_splits=folds)
+    else:
+        cv = KFold(n_splits=folds)
+
     try:
-        cv_results = cross_validate(model, predictors, target, scoring=scoring, cv=folds)
+        cv_results = cross_validate(model, predictors, target, scoring=scoring, cv=cv)
     except Exception as e:
         logging.exception("An error occurred during cross validation.")
         raise e
